abe4da684ac287e740c791ac6b15acb78b03e4db
uandras <40528937+uandras@users.noreply.github.com>
2018-11-04 23:45:50 +0100

    adding port

--- server.js
+++ server.js
@@ -63,8 +63,8 @@ app.post("/imageurl", (req, res) => {
   image.handleApiCall(req, res);
 });
 
-app.listen(3000, () => {
-  console.log("Server listening on port 3000");
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`Server listening on port ${process.env.PORT}`);
 });
 
 /*
